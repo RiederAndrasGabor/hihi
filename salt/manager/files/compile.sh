ee0efda21417aa2f3c311b0dcb4ceec6fa18b3ed
Guba Sándor <guba.sandor@cloud.bme.hu>
2014-10-10 18:10:57 +0200

    add support for pipeline

--- salt/manager/files/compile.sh
+++ salt/manager/files/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+cd /home/{{ pillar['user'] }}/circle/circle/
 source /home/{{ pillar['user'] }}/.virtualenvs/circle/bin/activate
 source /home/{{ pillar['user'] }}/.virtualenvs/circle/bin/postactivate
+bower install
 python /home/{{ pillar['user'] }}/circle/circle/manage.py collectstatic --noinput
 python /home/{{ pillar['user'] }}/circle/circle/manage.py compilemessages
