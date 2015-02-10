fff17377b4e300f724be014a42cbad56660a06dc
Istvan Gergely Olah <olah.istvan.gergely@gmail.com>
2015-02-10 23:42:38 +0100

    Remove su command from graphite syncdb

--- salt/graphite/files/syncdb.sh
+++ salt/graphite/files/syncdb.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
-su - {{ pillar['graphite']['user'] }}
 source /home/{{ pillar['graphite']['user'] }}/.virtualenvs/graphite/bin/activate;
 cd /opt/graphite/webapp/graphite/
 python manage.py syncdb --noinput
