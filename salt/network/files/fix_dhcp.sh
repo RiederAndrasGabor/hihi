5d8a4c22431269a18550a666e031d129cda91ea3
Czémán Arnold <czeman.arnold@cloud.bme.hu>
2015-07-28 00:28:30 +0200

    network: Fix vm interface up error

--- salt/network/files/fix_dhcp.sh
+++ salt/network/files/fix_dhcp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sed -i '/HWADDR=.*/d' /etc/sysconfig/network-scripts/ifcfg-vm
+sed -i -e \$aNM_CONTROLLED=\"no\" /etc/sysconfig/network-scripts/ifcfg-vm
 ifup vm
 systemctl restart firewall
 systemctl restart dhcpd
